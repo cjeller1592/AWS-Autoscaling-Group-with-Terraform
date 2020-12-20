7ea1f8325d96c9062fc257978b7e57c2871b2a89
CJ Eller <cjeller1592@gmail.com>
2020-12-20 17:47:11 -0500

    Added main.tf and README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# AWS Autoscaling Group with Terraform
+
+This is a lab that creates an Autoscaling Group in AWS using Terraform.
---
+++ main.tf
@@ -0,0 +1,58 @@
+provider "aws" {
+    region = "us-east-2"
+} 
+
+resource "aws_launch_template" "foobar" {
+  name_prefix   = "foobar"
+  image_id      = "ami-09558250a3419e7d0"
+  instance_type = "t2.micro"
+  key_name      = "my-key-pair"
+}
+
+resource "aws_autoscaling_group" "bar" {
+  availability_zones = ["us-east-2a", "us-east-2b"]
+  desired_capacity   = 1
+  max_size           = 3
+  min_size           = 1
+
+  launch_template {
+    id      = aws_launch_template.foobar.id
+    version = "$Latest"
+  }
+}
+
+resource "aws_autoscalingplans_scaling_plan" "example" {
+  name = "example-predictive-cost-optimization"
+
+  application_source {
+    tag_filter {
+      key    = "application"
+      values = ["example"]
+    }
+  }
+
+scaling_instruction {
+  #  disable_dynamic_scaling = true
+
+    max_capacity       = 5
+    min_capacity       = 1
+    resource_id        = format("autoScalingGroup/%s", aws_autoscaling_group.bar.name)
+    scalable_dimension = "autoscaling:autoScalingGroup:DesiredCapacity"
+    service_namespace  = "autoscaling"
+
+    target_tracking_configuration {
+      predefined_scaling_metric_specification {
+        predefined_scaling_metric_type = "ASGAverageCPUUtilization"
+      }
+
+      target_value = 5
+    }
+
+    predictive_scaling_max_capacity_behavior = "SetForecastCapacityToMaxCapacity"
+    predictive_scaling_mode                  = "ForecastAndScale"
+
+    predefined_load_metric_specification {
+      predefined_load_metric_type = "ASGTotalCPUUtilization"
+    }
+  }
+}
