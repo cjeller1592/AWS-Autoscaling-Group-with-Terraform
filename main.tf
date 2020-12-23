40e3619f40044e99d38227640e726e6f25921612
CJ Eller <cjeller1592@gmail.com>
2020-12-22 21:58:44 -0500

    Added Ansible playbook and script

--- main.tf
+++ main.tf
@@ -12,13 +12,20 @@ resource "aws_launch_template" "foobar" {
 resource "aws_autoscaling_group" "bar" {
   availability_zones = ["us-east-2a", "us-east-2b"]
   desired_capacity   = 1
-  max_size           = 3
+  max_size           = 5
   min_size           = 1
 
   launch_template {
     id      = aws_launch_template.foobar.id
     version = "$Latest"
   }
+
+  tag {
+    key                 = "foo"
+    value               = "bar"
+    propagate_at_launch = true
+  }
+
 }
 
 resource "aws_autoscalingplans_scaling_plan" "example" {
@@ -55,4 +62,9 @@ scaling_instruction {
       predefined_load_metric_type = "ASGTotalCPUUtilization"
     }
   }
+}
+
+output "ec2_ip" {
+  description = "IP address for EC2 instance in the Auto Scaling group"
+  value = aws_autoscaling_group.bar
 }
